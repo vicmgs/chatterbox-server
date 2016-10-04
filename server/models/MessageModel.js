5f03b6bd937c341367c8d7e4f0d8758893ab395e
vicmgs <vic.mgs@gmail.com>
2016-10-04 15:23:21 -0700

    Save message data on server side

--- server/models/MessageModel.js
+++ server/models/MessageModel.js
@@ -1,14 +1,23 @@
+var fs = require('fs');
+
 module.exports = {
-  messages: [],
+  messages: null,
 
   addMessage: function(message) {
     message.createdAt = new Date();
     message.updatedAt = new Date();
     message.objectId = this.messages.length;
+
     this.messages.push(message);
+    fs.writeFile('./server/data/message.js', JSON.stringify(this.messages), function(error) {
+      console.log(error);
+    });
   },
 
   getMessages: function() {
+    if (this.messages === null) {
+      this.messages = JSON.parse(fs.readFileSync('./server/data/message.js', 'utf-8'));
+    }
     if (this.messages.length < 100) {
       return this.messages;
     } else {
